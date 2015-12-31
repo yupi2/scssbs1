94a0fe1cef718f9e76a1a39e6806f5a54de622a7
yupi2 <yupi2@openmailbox.org>
2015-12-30 18:26:23 -0600

    initial DLLMain skeleton

---
+++ dllmain.cpp
@@ -0,0 +1,23 @@
+#include <Windows.h>
+//#include <iostream>
+
+BOOL WINAPI DllMain(
+	HINSTANCE hDLLHandle,
+	DWORD     nReason,
+	LPVOID    Reserved)
+{
+	BOOL bSuccess = TRUE;
+
+	switch (nReason)
+	{
+		case DLL_PROCESS_DETACH:
+
+		case DLL_PROCESS_ATTACH:
+
+		case DLL_THREAD_ATTACH:
+
+		case DLL_THREAD_DETACH:
+
+		
+	}
+}
