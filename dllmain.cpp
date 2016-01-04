5eee7550d099c4f57795352ba0d890df40663c62
yupi2 <yupi2@openmailbox.org>
2016-01-03 18:12:44 -0600

    let's get rid of all of this stupid crap also

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,58 +1,47 @@
 #include <Windows.h>
 //#include <iostream>
 
-#include "log.hpp"
-#include "hack.hpp"
+#define util_assert(x) if (!(x)) ExitProcess(1);
 
-static wchar_t const * const reason_strings[] = {
-	[DLL_PROCESS_DETACH] = L"process detach",
-	[DLL_PROCESS_ATTACH] = L"process attach",
-	[DLL_THREAD_ATTACH]  = L"thread attach",
-	[DLL_THREAD_DETACH]  = L"thread detach"
-};
+typedef void* (*CreateInterfaceFn)(const char *pName, int *pReturnCode);
 
-BOOL ProcessDetach(
-	HINSTANCE hDLL,
-	LPVOID    pReserved)
+DWORD WINAPI BHOPThreadProc(
+	LPVOID pParameter)
 {
-	(void)hDLL;
-	(void)pReserved;
-	// pReserved is NULL if FreeLibrary has been called or the DLL load
-	// failed and non-NULL if the process is terminating.
+	(void)pParameter;
 
-	scssbs::log::cleanup();
+	HMODULE clientModule;
+	HMODULE engineModule;
+	CreateInterfaceFn clientCreateInterface;
+	CreateInterfaceFn engineCreateInterface;
 
-	// Return value is ignored by DllMain.
-	return TRUE;
+	util_assert((clientModule = GetModuleHandleW(L"client.dll")));
+	util_assert((engineModule = GetModuleHandleW(L"engine.dll")));
+
+	util_assert((clientCreateInterface = reinterpret_cast<CreateInterfaceFn>(
+		GetProcAddress(clientModule, "CreateInterface"))));
+	util_assert((engineCreateInterface = reinterpret_cast<CreateInterfaceFn>(
+		GetProcAddress(engineModule, "CreateInterface"))));
+
+	//"VClient017"
+
+	// TODO: grab the LocalPlayer class.
+
+
+	return 0;
 }
 
-BOOL ProcessAttach(
-	HINSTANCE hDLL,
-	LPVOID    pReserved)
+BOOL setup_bhop()
 {
-	(void)hDLL;
-	(void)pReserved;
-	// pReserved is NULL for dynamic loads and non-NULL for static loads.
+	HANDLE hThread = CreateThread(NULL, 0, BHOPThreadProc,
+		NULL, CREATE_SUSPENDED, NULL);
 
-	return scssbs::hack::start();
-}
+	if (hThread == NULL)
+		return FALSE;
 
-BOOL ThreadAttach(
-	HINSTANCE hDLL)
-{
-	(void)hDLL;
-
-	// Return value is ignored by DllMain.
-	return TRUE;
-}
-
-BOOL ThreadDetach(
-	HINSTANCE hDLL)
-{
-	(void)hDLL;
-
-	// Return value is ignored by DllMain.
-	return TRUE;
+	DWORD ret = ResumeThread(hThread);
+	(void)CloseHandle(hThread);
+	return ret != static_cast<DWORD>(-1);
 }
 
 // For more info see link below:
@@ -62,37 +51,15 @@ BOOL WINAPI DllMain(
 	DWORD     nReason,
 	LPVOID    pReserved)
 {
-	BOOL bSuccess = TRUE;
-
-	if (!scssbs::log::setup())
-		return FALSE;
-
-	if (nReason <= DLL_THREAD_DETACH)
-		scssbs::log::info(L"DllMain called - Reason: $s16$", reason_strings[nReason]);
-	else
-		scssbs::log::info(L"DllMain called - Reason: $u32$", nReason);
-
 	switch (nReason)
 	{
 		case DLL_PROCESS_DETACH:
-			bSuccess = ProcessDetach(hDLL, pReserved);
+			// do something
 			break;
 		case DLL_PROCESS_ATTACH:
 			//(void)DisableThreadLibraryCalls(hDLL);
-			bSuccess = ProcessAttach(hDLL, pReserved);
-			break;
-		case DLL_THREAD_ATTACH:
-			bSuccess = ThreadAttach(hDLL);
-			break;
-		case DLL_THREAD_DETACH:
-			bSuccess = ThreadDetach(hDLL);
-			break;
+			return setup_bhop();
 	}
 
-	if (!bSuccess)
-	{
-		// cleanup
-	}
-
-	return bSuccess;
+	return TRUE;
 }
